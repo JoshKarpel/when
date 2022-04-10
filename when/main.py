e0d09060b134e90656f54ae30a0c1dfe7d6cf8cd
Josh Karpel <josh.karpel@gmail.com>
2022-04-09 20:10:24 -0500

    add clock animation

--- when/main.py
+++ when/main.py
@@ -1,8 +1,11 @@
+import math
 import re
 from dataclasses import dataclass
 from difflib import get_close_matches
+from math import ceil, cos, pi, sin
 from textwrap import dedent
-from typing import List, Optional, Sequence, Set, Tuple
+from time import sleep
+from typing import Iterator, List, Optional, Sequence, Set, Tuple
 
 import pendulum
 import pendulum.tz.timezone
@@ -10,8 +13,11 @@ from humanize import precisedelta
 from pendulum import UTC
 from pendulum.parsing import ParserError
 from pytzdata import get_timezones
+from rich.align import Align
 from rich.box import ROUNDED
-from rich.console import Console, ConsoleRenderable, RenderableType
+from rich.console import Console, ConsoleOptions, ConsoleRenderable, RenderableType, RenderResult
+from rich.live import Live
+from rich.style import Style
 from rich.table import Column, Table
 from rich.text import Text
 from typer import Argument, Exit, Option, Typer
@@ -72,6 +78,14 @@ def when(
             """
         ),
     ),
+    clock: bool = Option(
+        False,
+        help=dedent(
+            """\
+            Display an animated clock showing current local time instead of the normal output.
+            """
+        ),
+    ),
     version: Optional[bool] = Option(
         None,
         "--version",
@@ -83,35 +97,42 @@ def when(
         ),
     ),
 ) -> None:
-    """ """
     stdout = Console()
     stderr = Console(stderr=True)
 
     now = pendulum.now()
 
-    try:
-        target = parse_t(t) or now
-    except ParserError as e:
-        stderr.print(Text(str(e), style="red"))
-        raise Exit(1)
+    if clock:
+        with Live(
+            renderable=Clock(target=pendulum.now()), console=stdout, auto_refresh=True, screen=True
+        ) as live:
+            while True:
+                live.update(Clock(target=pendulum.now()))
+                sleep(0.1)
+    else:
+        try:
+            target = parse_t(t) or now
+        except ParserError as e:
+            stderr.print(Text(str(e), style="red"))
+            raise Exit(1)
 
-    available_timezones = set(get_timezones())
-    good_timezones, bad_timezones = partition(timezones, lambda tz: tz in available_timezones)
-    display_bad_timezone_help(stdout, available_timezones, bad_timezones)
+        available_timezones = set(get_timezones())
+        good_timezones, bad_timezones = partition(timezones, lambda tz: tz in available_timezones)
+        display_bad_timezone_help(stdout, available_timezones, bad_timezones)
 
-    display_timezones = {pendulum.timezone(tz) for tz in good_timezones}  # type: ignore
-    if add_utc:
-        display_timezones.add(UTC)
-    if add_local:
-        display_timezones.add(pendulum.local_timezone())  # type: ignore
+        display_timezones = {pendulum.timezone(tz) for tz in good_timezones}  # type: ignore
+        if add_utc:
+            display_timezones.add(UTC)
+        if add_local:
+            display_timezones.add(pendulum.local_timezone())  # type: ignore
 
-    rich_time = RichTime(
-        target=target,
-        now=now,
-        timezones=sorted(display_timezones, key=lambda tz: tz.utcoffset(now), reverse=True),
-    )
+        rich_time = RichTime(
+            target=target,
+            now=now,
+            timezones=sorted(display_timezones, key=lambda tz: tz.utcoffset(now), reverse=True),
+        )
 
-    stdout.print(rich_time)
+        stdout.print(rich_time)
 
 
 def display_bad_timezone_help(
@@ -210,3 +231,99 @@ class RichTime:
         wrapper.add_row(by_timezone, metadata)
 
         return wrapper
+
+
+TAU = 2 * math.pi
+
+
+@dataclass(frozen=True)
+class Clock:
+    target: pendulum.DateTime
+
+    def __rich_console__(self, console: Console, options: ConsoleOptions) -> RenderResult:
+        size = options.max_height - 2
+        clock = [[Text(" ") for _ in range(size * 2)] + [Text("\n")] for _ in range(size)]
+
+        center = size // 2
+        radius = center
+
+        clock[center][center * 2] = Text("✵", style=Style(color="white", bold=True))
+
+        for sixtieth in range(1, 61):
+            theta = fraction_to_clock_angle(sixtieth / 60)
+            x = round(cos(theta) * radius) + center
+            y = round(sin(theta) * radius) + center
+
+            if sixtieth % 5 == 0:
+                texts = [Text(c, style=Style(color="white")) for c in str(sixtieth // 5)]
+                clock[y][x * 2 : x * 2 + len(texts)] = texts
+            else:
+                clock[y][x * 2] = Text(".", style=Style(color="white"))
+
+        draw_hand(
+            clock,
+            center,
+            radius,
+            fraction_to_clock_angle(
+                (self.target.hour + self.target.minute / 60 + self.target.second / 3600) % 12 / 12
+            ),
+            Style(color="#7570b3"),
+            "H",
+        )
+        draw_hand(
+            clock,
+            center,
+            radius,
+            fraction_to_clock_angle((self.target.minute + self.target.second / 60) / 60),
+            Style(color="#d95f02"),
+            "M",
+        )
+        draw_hand(
+            clock,
+            center,
+            radius,
+            fraction_to_clock_angle(self.target.second / 60),
+            Style(color="#1b9e77"),
+            "S",
+        )
+
+        yield Align.center(
+            Text.assemble(*(row for row in (Text.assemble(*x) for x in clock))),
+            vertical="middle",
+        )
+
+
+def fraction_to_clock_angle(frac):
+    return (frac * TAU) - TAU / 4
+
+
+def draw_hand(clock, center, radius, theta, style, last):
+    second_x = round(cos(theta) * radius) + center
+    second_y = round(sin(theta) * radius) + center
+    dx = abs(center - second_x)
+    dy = abs(center - second_y)
+
+    if dx > dy:
+        start, stop = sorted((center, second_x))
+        slope = (second_y - center) / (second_x - center)
+        for x in range(start, stop):
+            y = round(slope * (x - center)) + center
+            if (x, y) == (center, center):
+                continue
+            clock[y][x * 2] = Text(".", style=style)
+    else:
+        start, stop = sorted((center, second_y))
+        if second_x != center:
+            slope = (second_y - center) / (second_x - center)
+            for y in range(start, stop):
+                x = round((y - center) / slope) + center
+                if (x, y) == (center, center):
+                    continue
+                clock[y][x * 2] = Text(".", style=style)
+        else:
+            for y in range(start, stop):
+                if (center, y) == (center, center):
+                    continue
+                clock[y][center * 2] = Text(".", style=style)
+
+    clock[second_y][second_x * 2] = Text(last, style=Style.chain(style, Style(bold=True)))
